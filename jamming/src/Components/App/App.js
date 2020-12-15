f457fb17e0117aabfebc9c5906359f2a25a2d3e8
mariaburmeister <mariaburmeister@gmail.com>
2020-12-15 14:02:34 +0100

    Add get users playlists and add save edit to save

--- jamming/src/Components/App/App.js
+++ jamming/src/Components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
+import PlaylistStack from '../PlaylistStack/PlaylistStack'
 import Spotify from '../../util/Spotify';
 import './App.css';
   
@@ -10,11 +11,18 @@ class App extends React.Component {
     super(props);
 
     this.state = {
+      searchTerm: '',
       searchResults: [],
 
+      playlistID: '',
       playlistName: 'New Playlist',
-      playlistTracks: []
+      playlistTracks: [],
+
+      savedPlaylists: []
     };
+
+    this.getSavedPlaylists =
+    this.getSavedPlaylists.bind(this);
 
     this.addTrack = 
     this.addTrack.bind(this);
@@ -28,6 +36,9 @@ class App extends React.Component {
     this.savePlaylist = 
     this.savePlaylist.bind(this);
 
+    this.choosePlaylistFromStack = 
+    this.choosePlaylistFromStack.bind(this);
+
     this.search =
     this.search.bind(this);
   }
@@ -39,9 +50,14 @@ class App extends React.Component {
 
     const tracks = 
     [...this.state.playlistTracks, newTrack];
-    
 
     this.setState({playlistTracks: tracks});
+
+    const filteredResults = 
+    this.state.searchResults
+    .filter(track => track.id !== newTrack.id);
+
+    this.setState({searchResults: filteredResults});
   }
 
   removeTrack(trackToRemove) {
@@ -49,7 +65,9 @@ class App extends React.Component {
     this.state.playlistTracks
     .filter(track => track.id !== trackToRemove.id);
 
-    this.setState({playlistTracks: tracks})
+    this.setState({playlistTracks: tracks});
+
+    this.search(this.state.searchTerm);
   }
 
   updatePlaylistName(newName) {
@@ -59,23 +77,62 @@ class App extends React.Component {
   savePlaylist() {
     const trackURIs = 
     this.state.playlistTracks.map(track => track.uri);
-    Spotify.saveNewPlaylistToAccount( 
-      this.state.playlistName, 
-      trackURIs)
-      .then(() => {
-        this.setState({playlistName:'New Playlist'});
-        this.setState({playlistTracks:[]});
-      });
+
+    if (this.state.playlistID) {
+      console.log('isEdit');
+      Spotify.savePlaylistEdit(
+        this.state.playlistID, 
+        this.state.playlistName, 
+        trackURIs);
+    } else {
+      console.log('isNew');
+      console.log('playlistID' + this.state.playlistID);
+      Spotify.saveNewPlaylistToAccount( 
+        this.state.playlistName, 
+        trackURIs);
+    }
+    this.setState({playlistName:'New Playlist'});
+    this.setState({playlistTracks:[]});
+    setTimeout(() => this.getSavedPlaylists(), 2000);
+  }
+
+  choosePlaylistFromStack(savedPlaylist) {
+
+    this.setState({playlistName: savedPlaylist.name});
+    this.setState({playlistTracks: savedPlaylist.tracks});
+    this.setState({playlistID: savedPlaylist.id});
+
+    const newStack = 
+    this.state.savedPlaylists.filter((playlist) => playlist !== savedPlaylist);
+
+    this.setState({savedPlaylists: newStack});
   }
 
   search(term) { 
-    Spotify.search(term).then((searchResults) =>{ 
-      this.setState({searchResults: searchResults})
-    })
+    if(!term) {
+      return;
+    }
+
+    Spotify.search(term).then((searchResults) => { 
+      const filteredResults = searchResults.filter((track) => {
+        return !this.state.playlistTracks
+        .find(playlistTrack => track.id === playlistTrack.id);
+      });
+      this.setState({searchResults: filteredResults});
+      this.setState({searchTerm: term});
+    });
+  }
+
+  getSavedPlaylists() {
+    Spotify.getUsersPlaylists().then((playlists) => {
+      this.setState({savedPlaylists: playlists});
+    });
+    
   }
 
   componentDidMount() {
     window.addEventListener('load', () => {Spotify.getUserAccessToken()});
+    this.getSavedPlaylists();
   }
 
   render() {
@@ -98,9 +155,15 @@ class App extends React.Component {
               onSave={this.savePlaylist}
             />
           </div>
+          <PlaylistStack 
+            playlists={this.state.savedPlaylists}
+            onChoose={this.choosePlaylistFromStack}
+            isEditable={true}
+          />
         </div>
       </div>
     );
+
   }
 }
 
